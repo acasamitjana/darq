6c0717ee6a874552a26d6fc3844828b91d3cbcea
Toni <tonicc2@icloud.com>
2026-06-16 12:49:19 +0200

    He aconseguit crear el ReadTheDocs, quedaria personalitzarlo com nosaltres volguem

---
+++ docs/source/api/darq.src.rst
@@ -0,0 +1,45 @@
+darq.src package
+================
+
+Submodules
+----------
+
+darq.src.datasets module
+------------------------
+
+.. automodule:: darq.src.datasets
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+darq.src.models module
+----------------------
+
+.. automodule:: darq.src.models
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+darq.src.preprocessing module
+-----------------------------
+
+.. automodule:: darq.src.preprocessing
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+darq.src.results module
+-----------------------
+
+.. automodule:: darq.src.results
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: darq.src
+   :members:
+   :show-inheritance:
+   :undoc-members:
---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from pathlib import Path
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+ROOT = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(ROOT))
+os.environ["PYTHONPATH"] = str(ROOT)
+
+project = 'DARQ'
+copyright = '2026, Adrià Casamitjana'
+author = 'Adrià Casamitjana'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+autodoc_typehints = "description"
+autosummary_generate = True
